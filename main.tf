44ccb43f53f0a51a18f7f5d0dfeb0d1da4a6fc9b
Jayanta Saha <63189627+Jayanta89@users.noreply.github.com>
2023-11-23 18:39:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
-      version = ">= 3.59.0"
+      "source" = "hashicorp/azurerm"
+      "version" = ">= 3.59.0"
     } 
   }
   required_version = ">= 0.14.9"
